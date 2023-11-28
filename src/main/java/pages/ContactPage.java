e9beffe6fe43ea3a6ac202c725432f01b57d4047
Irina <irinadaniela.geanta@gmail.com>
2023-11-28 21:33:01 +0200

    curs24 final

--- src/main/java/pages/ContactPage.java
+++ src/main/java/pages/ContactPage.java
@@ -6,4 +6,13 @@ import utils.SeleniumWrappers;
 public class ContactPage extends SeleniumWrappers {
 
     public By nameField = By.cssSelector("input[name='your-name']");
+    public By zoomInMap = By.xpath("//button[@title='Zoom in']");
+    public By zoomOutMap = By.xpath("//button[@title='Zoom out']");
+    public By frame = By.tagName("iframe");
+
+
+    public void zoomMap(By locator){
+        driver.switchTo().frame(returnElement(frame));
+        click(locator);
+    }
 }
